0c353d6740f872aecfc25bdf38a77b9263270d77
bakulya <baktygul.bishkek@icloud.com>
2025-03-15 20:03:01 +0600

    updated

--- Afisha/urls.py
+++ Afisha/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from movie_app import views
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movie_app.urls')),
